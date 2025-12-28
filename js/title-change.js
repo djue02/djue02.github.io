1012f561b0dba4a479cfebdf403d5ef4d4c9f5ef
djue02 <djue02@gmail.com>
2025-12-28 20:48:27 +0800

    Site updated: 2025-12-28 20:48:26

--- js/title-change.js
+++ js/title-change.js
@@ -1,17 +1,16 @@
-// 标签页标题切换效果
+<script>
 var OriginTitle = document.title;
 var titleTime;
 
 document.addEventListener('visibilitychange', function () {
     if (document.hidden) {
-        // 页面被隐藏时
-        document.title = '你去哪啦 (@* ○ *@)';
+        document.title = '你去哪啦 (✿◡‿◡)';
         clearTimeout(titleTime);
     } else {
-        // 页面被激活时
-        document.title = '你回来啦 (^▽^*)';
+        document.title = '你回来啦 (￣▽￣)～■干杯□～(￣▽￣)';
         titleTime = setTimeout(function () {
             document.title = OriginTitle;
-        }, 2000); // 2秒后恢复原标题
+        }, 2000);
     }
 });
+</script>
