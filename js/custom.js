ded0fa7d7116a06bba920716912e5a62104db5c0
djue02 <djue02@gmail.com>
2026-01-07 01:02:34 +0800

    Site updated: 2026-01-07 01:02:34

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,7 @@
 jQuery(document).ready(function($) {
-    // 1. 移除分类列表链接的 title 属性 (之前的代码)
+    // 1. 移除分类列表链接的 title 属性
     $(".list-group-item").removeAttr("title");
 
-    // 2. 关闭搜索框的浏览器自动填充/历史记录 (新增的代码)
+    // 2. 关闭搜索框的自动填充
     $("#local-search-input").attr("autocomplete", "off");
-});
 
